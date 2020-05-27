ad1e92f3a10ba745a2dcdff1f31ea318ae5cce41
obajitheking <62754731+obajitheking@users.noreply.github.com>
2020-05-28 00:33:05 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,52 +1,29 @@
-let quotes = [
-    {
-        "quote": "Believe in yourself! Have faith in your abilities!" + "<br>" + "Without a humble but reasonable confidence in your own powers you cannot be successful or happy.",
-        "author": "Norman Vincent Peale"
-    }, {
-        "quote": "Press forward. Do not stop, do not linger in your journey,but strive for the mark set before you.",
-        "author": "George Whitefield"
-    }, {
-        "quote": "The future belongs to those who believe in the beauty of their dreams.",
-        "author": "Eleanor Roosevelt"
-    }, {
-        "quote": "Aim for the moon. If you miss, you may hit a star",
-        "author": "W. Clement Stone"
-    }, {
-        "quote": "Never look back unless you're planning to go that way.",
-        "author": "Henry David Thoreau"
-    }, {
-        "quote": "There are chords in the heart of the most reckless which cannot be touched without emotion.",
-        "author": "Edgar Allan Poe"
-    }, {
-        "quote": "Toba doko wa ma lole, Bye Bye.",
-        "author": "Naira Marley"
-    }, {
-        "quote": "Live as, everyday is your last.",
-        "author": "Edgar The King"
-    }
-    
-]
-
-var time = 3000; 
 var i = 0;
-const btn = document.getElementById("btn");
-const quote = document.getElementById("quote");
-const author = document.getElementById("author");
+var images = [];
 
 
+images[0] = 'one.png';
+images[1] = 'two.png';
+images[2] = 'three.png';
+images[3] = 'four.png';
+images[4] = 'five.png';
+images[5] = 'six.png';
+images[6] = 'seven.png';
+images[7] = 'eight.png';
+images[8] = 'nine.png';
 
-btn.addEventListener('click', getQuote);
+function changeImg() {
 
-function getQuote() {
+    let number = Math.floor(Math.random() * images.length);
 
-    let number = Math.floor(Math.random() * quotes.length);
+    document.slide.src = images[number]; 
 
-    quote.innerHTML = '<span>"</span>' + quotes[number].quote + '<span>"</span>';
-    author.innerHTML = '<span>--</span>' + quotes[number].author; 
-    
+  
 
-    setTimeout("changeImg", time);
+    btn.innerHTML = 'Click here for more'
 
 }
 
+const btn = document.querySelector('.btn');
 
+btn.addEventListener('click', changeImg);
